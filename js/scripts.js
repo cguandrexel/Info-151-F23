1f74adf847ed90e79a3ba855a8a73f352920336a
cguandrexel <chun.guan@drexel.edu>
2023-11-15 22:41:43 -0600

    debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,22 @@
 
-
 var mainNav = document.querySelector("#mainNav");
 
 var burgerBtn = document.querySelector("#burger-container button");
+// console.log(burgerBtn);
 
 var canYouSeeMainNav = false;
 
 function showNav(){
 
-    if(canYouSeeMainNav === false){
-        // console.log("show menu");
-
+    if (canYouSeeMainNav === false) {
+        console.log("show menu");
         mainNav.classList.add("showMainNav");
         canYouSeeMainNav = true;
-    }else{
-        // console.log("hide menu");
-
+    } else {
+        console.log("hide menu");
         mainNav.classList.remove("showMainNav");
         canYouSeeMainNav = false;
     }
-    
 }
 
-burgerBtn.addEventListener("click", showNav);
+burgerBtn.addEventListener("click", showNav());
