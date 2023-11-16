a9f7e1d884dd0c3e4de85c585e3c0bcc906c40b9
cguandrexel <chun.guan@drexel.edu>
2023-11-15 22:18:44 -0600

    debugging

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,25 @@
+
+
 var mainNav = document.querySelector("#mainNav");
 
-var burgerBtn = document.querySelector("#burger-container button")
-// console.log(burgerBtn);
-// this can only be true or false known as a boolean
+var burgerBtn = document.querySelector("#burger-container button");
+
 var canYouSeeMainNav = false;
 
-// var sectionTags = document.querySelectorAll(".myClass");
-
 function showNav(){
-    // console.log("button clicked");
+
     if(canYouSeeMainNav === false){
-        console.log("show menu");
-        // this will add the class of showMainNav to the id mainNav
+        // console.log("show menu");
+
         mainNav.classList.add("showMainNav");
         canYouSeeMainNav = true;
     }else{
-        console.log("hide menu");
-        // this will remove the class of showMainNav to the id mainNav
+        // console.log("hide menu");
+
         mainNav.classList.remove("showMainNav");
         canYouSeeMainNav = false;
     }
     
 }
 
-// add a event listener to the button
 burgerBtn.addEventListener("click", showNav);
