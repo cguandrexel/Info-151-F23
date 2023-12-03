b4145e8834b84cef963a5d2aca45d3e028791166
cguandrexel <chun.guan@drexel.edu>
2023-12-02 22:48:53 -0600

    Revised Week 7 Config
    Revised Week 7 page with professor's codes

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,51 @@
 
+// var mainNav = document.querySelector("#mainNav");
+
+// var burgerBtn = document.querySelector("#burger-container button");
+
+// var canYouSeeMainNav = false;
+
+// function showNav(){
+
+//     if (canYouSeeMainNav === false) {
+//         console.log("show menu");
+//         mainNav.classList.add("showMainNav");
+//         canYouSeeMainNav = true;
+//     } else {
+//         console.log("hide menu");
+//         mainNav.classList.remove("showMainNav");
+//         canYouSeeMainNav = false;
+//     }
+// }
+// burgerBtn.addEventListener("click", showNav);
+
 var mainNav = document.querySelector("#mainNav");
 
-var burgerBtn = document.querySelector("#burger-container button");
-
+var burgerBtn = document.querySelector("#burger-container button")
+// console.log(burgerBtn);
+// this can only be true or false known as a boolean
 var canYouSeeMainNav = false;
 
-function showNav(){
+// var sectionTags = document.querySelectorAll(".myClass");
 
-    if (canYouSeeMainNav === false) {
+function showNav(){
+    // console.log("button clicked");
+    if(canYouSeeMainNav === false){
         console.log("show menu");
+        // this will add the class of showMainNav to the id mainNav
         mainNav.classList.add("showMainNav");
         canYouSeeMainNav = true;
-    } else {
+    }else{
         console.log("hide menu");
+        // this will remove the class of showMainNav to the id mainNav
         mainNav.classList.remove("showMainNav");
         canYouSeeMainNav = false;
     }
+    
 }
+
+// add a event listener to the button
 burgerBtn.addEventListener("click", showNav);
+
+// will call a function with the name of showNav
+// showNav();
